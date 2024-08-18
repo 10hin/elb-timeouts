630a455eecc87c1c51427639d751ea72cfd73078
10hin <10hin@outlook.jp>
2024-08-19 00:48:44 +0900

    fix to use terraform command not alias

--- start-session.bash
+++ start-session.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 target="$( \
-  tf -chdir=terraform/ show -json \
+  terraform -chdir=terraform/ show -json \
     | jq -r '.values.root_module.resources[] | select(.address == "aws_instance.app") | .values.id' \
 )"
 if (( "${#target}" <= 0 ))
